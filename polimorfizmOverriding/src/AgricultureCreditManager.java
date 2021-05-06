7b720e316018dd29cbd89a85eef0ab5331276997
Yaren Ulusoy <ulusoyyaren@gmail.com>
2021-05-06 19:06:56 +0300

    Delete polimorfizmOverriding/src directory

--- polimorfizmOverriding/src/AgricultureCreditManager.java
+++
@@ -1,2 +0,0 @@
-public class AgricultureCreditManager extends  BaseCreditManager{
-}
--- polimorfizmOverriding/src/TeacherCreditManager.java
+++
@@ -1,5 +0,0 @@
-public class TeacherCreditManager extends BaseCreditManager{
-    public double hesapla(double tutar){ //override
-        return tutar * 1.10;
-    }
-}
